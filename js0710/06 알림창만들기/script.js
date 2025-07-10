7ad7451cc77a521f8e13a50812f4271ec1dc6c3c
jjop2 <jngsuh616@gmail.com>
2025-07-10 16:04:26 +0900

    js0710 06 알림창만들기

---
+++ js0710/06 알림창만들기/script.js
@@ -0,0 +1,8 @@
+function show(on) {
+    document.getElementById('ab').style.display = on;
+}
+
+// 변수명이 자바에서 쓰는 거면 오류날 수 있음=hidden 등등... 변수명 바꿔보기
+// function hide() {
+//   document.getElementById('ab').style.display = 'none';
+// }
