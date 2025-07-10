797c268eb5c25a90dc47a041ff2f1b4b355711e9
jjop2 <jngsuh616@gmail.com>
2025-07-10 16:38:23 +0900

    js0710 06 알림창만들기.1.1

--- js0710/06 알림창만들기/script.js
+++ js0710/06 알림창만들기/script.js
@@ -1,8 +1,14 @@
-function show(on) {
-    document.getElementById('ab').style.display = on;
+function show(state) {
+    document.getElementById('ab').style.display = state;
 }
 
 // 변수명이 자바에서 쓰는 거면 오류날 수 있음=hidden 등등... 변수명 바꿔보기
 // function hide() {
 //   document.getElementById('ab').style.display = 'none';
 // }
+
+// 알림창1,2,3 누르면 서로 다른 메세지가 보이도록 구현
+
+function msg(num) {
+  document.querySelector('.alert').innerHTML = `알림창${num}을 눌렀다`;
+};
