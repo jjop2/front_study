d2d2cd7f5b65a065282a05999a8b25ffaf96a8a3
jjop2 <jngsuh616@gmail.com>
2025-07-10 16:47:41 +0900

    js0710 06 알림창만들기.1.2 - jQuery문법 추가

--- js0710/06 알림창만들기/script.js
+++ js0710/06 알림창만들기/script.js
@@ -1,5 +1,9 @@
 function show(state) {
-    document.getElementById('ab').style.display = state;
+  // 바닐라 JS 문법
+  document.getElementById('ab').style.display = state;
+  
+  // jQuery 문법
+  // $('#ab').css( 'display', state );
 }
 
 // 변수명이 자바에서 쓰는 거면 오류날 수 있음=hidden 등등... 변수명 바꿔보기
@@ -10,5 +14,10 @@ function show(state) {
 // 알림창1,2,3 누르면 서로 다른 메세지가 보이도록 구현
 
 function msg(num) {
-  document.querySelector('.alert').innerHTML = `알림창${num}을 눌렀다`;
+  // JS문법
+  // document.querySelector('.alert').innerHTML = `알림창${num}을 눌렀다`;
+
+  // jQuery 문법
+  $('.alert').html(`알림창${num}을 눌렀다`);
 };
+
