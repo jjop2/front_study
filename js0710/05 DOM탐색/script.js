54a2ec8e0bf0834a14250f0229973acc7e61f8ff
jjop2 <jngsuh616@gmail.com>
2025-07-10 15:05:55 +0900

    js0710 05 DOM탐색

---
+++ js0710/05 DOM탐색/script.js
@@ -0,0 +1,8 @@
+let box = document.getElementById('box')
+box.style.background= "red";
+console.log(box);
+
+// 선택자를 이용해서 요소를 선택할 수 있음
+const box2 = document.querySelector('#box')
+box2.style.color = 'white';
+console.log(box2);
