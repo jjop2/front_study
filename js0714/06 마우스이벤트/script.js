40b3ad68dc51f5964e0d5628d5ff2af6b4d5ee37
jjop2 <jngsuh616@gmail.com>
2025-07-14 16:50:03 +0900

    js0714 06마우스이벤트

--- js0714/06 마우스이벤트/script.js
+++ js0714/06 마우스이벤트/script.js
@@ -18,12 +18,16 @@ document.querySelector('button').addEventListener('click', (e) => {
 })
 
 
-
+// change : 변경되고 포커스가 나가면 작동
 document.querySelector('input').addEventListener('change', () => {
   alert('change 이벤트 작동됨');
 })
 
 
+// input : 변경 감지되는 모든 순간 작동
 document.querySelector('input').addEventListener('input', () => {
   console.log('input 이벤트 작동')
 })
+
+// cut : 잘라내기하는 순간 작동
+// copy : 복사, paste : 붙여넣기
